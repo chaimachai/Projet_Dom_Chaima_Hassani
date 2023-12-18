5b9a0d754f5251002d0650b7a6123eced263b8d3
chaima <hassanichaima@outlook.be>
2023-12-18 14:32:31 +0100

    section 4

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,16 @@
 //.. Variables
+//body
+let body = document.body;
 
-//nav
+// header
+let header = document.querySelector("header");
+
+// nav
 let menu_burger = document.querySelector(".menu_burger");
 let nav_burger = document.querySelector(".nav");
 let li_dropdown = document.querySelector(".li_dropdown");
 let dropdown = document.querySelector(".dropdown");
+let nav = document.querySelector("nav");
 
 //.. Events
 
@@ -14,9 +20,10 @@ menu_burger.addEventListener("click",()=>{
     nav_burger.classList.toggle("nav_none");
 })
 li_dropdown.addEventListener("click",()=>{
-    dropdown.classList.toggle("none")
-    li_dropdown.querySelector("span").classList.toggle("fa-chevron-down")
-    li_dropdown.querySelector("span").classList.toggle("fa-chevron-up")
+    dropdown.classList.toggle("none");
+    li_dropdown.querySelector("span").classList.toggle("fa-chevron-down");
+    li_dropdown.querySelector("span").classList.toggle("fa-chevron-up");
 })
+
 
 
