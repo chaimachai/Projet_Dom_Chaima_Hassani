54257b21262dbf897f06d54c37a434919dffeb2b
chaima <hassanichaima@outlook.be>
2023-12-18 16:16:21 +0100

    section 5

--- public/js/main.js
+++ public/js/main.js
@@ -12,6 +12,11 @@ let li_dropdown = document.querySelector(".li_dropdown");
 let dropdown = document.querySelector(".dropdown");
 let nav = document.querySelector("nav");
 
+// section 5
+let carousel = document.querySelector(".contenu_testimonials");
+let carousel_item = document.querySelectorAll(".carousel_item");
+let btn = document.querySelector("#section_5").querySelectorAll("button");
+
 //.. Events
 
 // nav
@@ -25,5 +30,85 @@ li_dropdown.addEventListener("click",()=>{
     li_dropdown.querySelector("span").classList.toggle("fa-chevron-up");
 })
 
+//section 5
+btn.forEach(elements => {
+    elements.addEventListener("click",(e)=>{
+        if (e.target ==  btn[0]){
+            btn.forEach(element => {
+                if(element.className.includes("actif")){
+                    if(element == btn[1]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[2]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[3]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[4]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }
+                }
+            })
+        }
+        if (e.target ==  btn[1]){
+            btn.forEach(element => {
+                if(element.className.includes("actif")){
+                    if(element == btn[0]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                    }else if (element == btn[2]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[3]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[4]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }
+                }
+            })
+        }
+        if (e.target ==  btn[2]){
+            btn.forEach(element => {
+                if(element.className.includes("actif")){
+                    if(element == btn[0]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                    }else if (element == btn[1]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                    }else if (element == btn[3]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[4]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }
+                }
+            })
+        }
+        if (e.target ==  btn[3]){
+            btn.forEach(element => {
+                if(element.className.includes("actif")){
+                    if(element == btn[0]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                    }else if (element == btn[1]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                    }else if (element == btn[2]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                    }else if (element == btn[4]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }
+                }
+            })
+        }
+        if (e.target ==  btn[4]){
+            btn.forEach(element => {
+                if(element.className.includes("actif")){
+                    if(element == btn[0]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3 
+                    }else if (element == btn[1]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[2]){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                    }else if (element == btn[3]){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                    }
+                }
+            })
+        }
+    })
+})
 
 
