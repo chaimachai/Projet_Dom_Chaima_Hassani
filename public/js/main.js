e9c6f7c4cc1e0d1d6d046be39f8a520ec0f9eec2
chaima <hassanichaima@outlook.be>
2023-12-19 09:11:36 +0100

    changement icon

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 //.. Variables
-//body
+// body
 let body = document.body;
 
 // header
@@ -16,6 +16,21 @@ let nav = document.querySelector("nav");
 let carousel = document.querySelector(".contenu_testimonials");
 let carousel_item = document.querySelectorAll(".carousel_item");
 let btn = document.querySelector("#section_5").querySelectorAll("button");
+
+// section 6
+let h4 = document.querySelector(".h4").querySelectorAll("h4");
+let tabs = document.querySelectorAll(".tabs");
+
+// section 7
+let h2 = document.querySelector(".cards_7").querySelectorAll("h2");
+let btn_oval = document.querySelector(".oval");
+let p_month = document.querySelector(".p_month");
+let p_year = document.querySelector(".p_year")
+let month = true
+
+// section 8
+let texte_accordion = document.querySelectorAll(".texte_accordion");
+let div_accordion = document.querySelectorAll(".accordion")
 
 //.. Events
 
@@ -35,80 +50,155 @@ btn.forEach(elements => {
     elements.addEventListener("click",(e)=>{
         if (e.target ==  btn[0]){
             btn.forEach(element => {
-                if(element.className.includes("actif")){
-                    if(element == btn[1]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[2]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[3]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[4]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
+                if(element.className.includes("active")){
+                    if(element.className.includes("btn2")){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn3")){
+                        carousel.scrollLeft -= 2*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn4")){
+                        carousel.scrollLeft -= 3*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn5")){
+                        carousel.scrollLeft -= 4*carousel.clientWidth / 3
+                        element.classList.remove("active")
                     }
                 }
             })
         }
         if (e.target ==  btn[1]){
             btn.forEach(element => {
-                if(element.className.includes("actif")){
-                    if(element == btn[0]){
+                if(element.className.includes("active")){
+                    if(element.className.includes("btn1")){
+                        carousel.scrollLeft += carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn3")){
+                        console.log("test3");
                         carousel.scrollLeft -= carousel.clientWidth / 3
-                    }else if (element == btn[2]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[3]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[4]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn4")){
+                        console.log("test4");
+                        carousel.scrollLeft -= 2*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn5")){
+                        console.log("test5");
+                        carousel.scrollLeft -= 3*carousel.clientWidth / 3
+                        element.classList.remove("active")
                     }
                 }
             })
         }
         if (e.target ==  btn[2]){
             btn.forEach(element => {
-                if(element.className.includes("actif")){
-                    if(element == btn[0]){
-                        carousel.scrollLeft -= carousel.clientWidth / 3
-                    }else if (element == btn[1]){
-                        carousel.scrollLeft -= carousel.clientWidth / 3
-                    }else if (element == btn[3]){
+                if(element.className.includes("active")){
+                    if(element.className.includes("btn1")){
+                        carousel.scrollLeft += 2*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn2")){
                         carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[4]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn4")){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn5")){
+                        carousel.scrollLeft -= 2*carousel.clientWidth / 3
+                        element.classList.remove("active")
                     }
                 }
             })
         }
         if (e.target ==  btn[3]){
             btn.forEach(element => {
-                if(element.className.includes("actif")){
-                    if(element == btn[0]){
-                        carousel.scrollLeft -= carousel.clientWidth / 3
-                    }else if (element == btn[1]){
-                        carousel.scrollLeft -= carousel.clientWidth / 3
-                    }else if (element == btn[2]){
-                        carousel.scrollLeft -= carousel.clientWidth / 3
-                    }else if (element == btn[4]){
+                if(element.className.includes("active")){
+                    if(element.className.includes("btn1")){
+                        carousel.scrollLeft += 3*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn2")){
+                        carousel.scrollLeft += 2*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn3")){
                         carousel.scrollLeft += carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn5")){
+                        carousel.scrollLeft -= carousel.clientWidth / 3
+                        element.classList.remove("active")
                     }
                 }
             })
         }
         if (e.target ==  btn[4]){
             btn.forEach(element => {
-                if(element.className.includes("actif")){
-                    if(element == btn[0]){
-                        carousel.scrollLeft -= carousel.clientWidth / 3 
-                    }else if (element == btn[1]){
+                if(element.className.includes("active")){
+                    if(element.className.includes("btn1")){
+                        carousel.scrollLeft += 4*carousel.clientWidth / 3 
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn2")){
+                        carousel.scrollLeft -= 3*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn3")){
+                        carousel.scrollLeft -= 2*carousel.clientWidth / 3
+                        element.classList.remove("active")
+                    }else if (element.className.includes("btn4")){
                         carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[2]){
-                        carousel.scrollLeft += carousel.clientWidth / 3
-                    }else if (element == btn[3]){
-                        carousel.scrollLeft -= carousel.clientWidth / 3
+                        element.classList.remove("active")
                     }
                 }
             })
         }
+        e.target.classList.add("active")
     })
 })
 
+//section 6
+h4.forEach(element => {
+    element.addEventListener("click",()=>{
+        tabs.forEach(element =>{
+            element.classList.add("none");
+        })
+        document.querySelector(`.div_${element.className}`).classList.remove("none");
+    })
+})
 
+//section 7
+btn_oval.addEventListener("click",()=>{
+    if(month){
+        h2.forEach(element => {
+            element.innerHTML = element.innerHTML*10;
+        })
+        month = false;
+        btn_oval.firstElementChild.classList.remove("month");
+        btn_oval.firstElementChild.classList.add("year");
+        p_month.classList.remove("actif");
+        p_year.classList.add("actif");
+    }else{
+        h2.forEach(element => {
+            element.innerHTML = element.innerHTML/10;
+        })
+        month = true;
+        btn_oval.firstElementChild.classList.remove("year");
+        btn_oval.firstElementChild.classList.add("month");
+        p_month.classList.add("actif");
+        p_year.classList.remove("actif");
+    }
+})
+
+// section 8
+div_accordion.forEach(element => {
+    element.addEventListener("click",()=>{
+        div_accordion.forEach(elements => {
+            if(elements.className.includes("actif")){
+                if(element != elements)
+                elements.classList.remove("actif");
+                elements.classList.add("actifno");
+            }
+        })
+        if (element.className.includes("actif")){
+            element.classList.add("actifno")
+            element.classList.remove("actif");
+        }else{
+            element.classList.remove("actifno")
+            element.classList.add("actif");
+        }
+    })
+})
